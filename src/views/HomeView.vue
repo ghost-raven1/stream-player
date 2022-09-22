6565419577c9e98974490ea3e650d852d5891d44
ghost1 <sonofagronom@gmail.com>
2022-09-22 17:25:10 +0700

    added fixed errors && added styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,10 @@
   <div class="home">
     <div class="home__container container">
       <StreamPlayer :link="link" class="container__player" />
-      <input v-model="currentLink" class="container__input" />
-      <button @click="link = currentLink">Change stream</button>
+      <div class="container__stream stream">
+        <input v-model="currentLink" class="stream__input" />
+        <button class="stream__button" @click="link = currentLink">Change stream</button>
+      </div>
     </div>
   </div>
 </template>
@@ -24,6 +26,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.stream {
+  display: grid;
+  grid-template-columns: 8fr 3fr;
+  width: 100%;
+  &__button {
+    background: #3F888F;
+    border-radius: 50px;
+    padding: 15px;
+    border: none;
+    transition: .2s;
+    box-shadow: 0 5px 10px 2px rgba(34, 60, 80, 0.2);
+    font-size: 16px;
+    color: white;
+    &:hover {
+      cursor: pointer;
+      background: #5cbcc4;
+    }
+  }
+  &__input {
+    font-size: 16px;
+    padding-left: 10px;
+    margin-right: 10px;
+    border-radius: 16px;
+  }
+}
 .home {
   display: flex;
   width: 100%;
